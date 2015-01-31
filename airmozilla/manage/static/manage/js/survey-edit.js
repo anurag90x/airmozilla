f679e0ab7c37b68eefe4608d525d040fffa0e0a8
Anurag Chaudhury <anuragchaudhury@gmail.com>
2015-01-31 04:19:41 +0400

    Bug 1117105 - Better ajax feedback

--- airmozilla/manage/static/manage/js/survey-edit.js
+++ airmozilla/manage/static/manage/js/survey-edit.js
@@ -31,19 +31,28 @@ $(function() {
     // hijack the ability to save/edit
     $('form.edit').submit(function() {
         var form = $(this);
+        $(form.children('button')).text('Saving..');
         var tr = form.closest('tr');
         var textarea = $('textarea', tr);
+        $(textarea).attr('readonly','readonly');
         var data = {
             csrfmiddlewaretoken: $('input[name="csrfmiddlewaretoken"]').val()
         };
         data[textarea.attr('name')] = textarea.val();
         $.post(form.attr('action'), data)
         .then(function(response) {
+
+            $(textarea).removeAttr('readonly');
             if (response.error) {
                 alert('Unable to save. Error: ' + response.error);
             } else {
                 textarea.val(response.question);
+                $(form.children('button')).text('Saved');
             }
+            setTimeout(function()
+            {
+                $(form.children('button')).text('Save');
+            }, 2*1000);
         }).fail(function() {
             console.warn(arguments);
             alert('Unable to complete the save right now.');
